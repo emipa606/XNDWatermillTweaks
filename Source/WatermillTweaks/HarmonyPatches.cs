2fc702c13877e48728dc611384363f8c15614443
Mlie <emipa606@users.noreply.github.com>
2020-04-10 22:20:47 +0200

    First publish

---
+++ Source/WatermillTweaks/HarmonyPatches.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace WatermillTweaks
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+
+        private static readonly Type patchType = typeof(HarmonyPatches);
+
+        static HarmonyPatches()
+        {
+            var h = new Harmony("XeoNovaDan.WatermillTweaks");
+
+            h.Patch(AccessTools.Property(typeof(CompPowerPlantWater), "DesiredPowerOutput").GetGetMethod(true), null,
+                new HarmonyMethod(patchType, nameof(PostfixDesiredPowerOutput)));
+
+            h.Patch(AccessTools.Method(typeof(CompPowerPlantWater), nameof(CompPowerPlantWater.CompInspectStringExtra)), null,
+                new HarmonyMethod(patchType, nameof(PostfixCompInspectStringExtra)));
+        }
+
+        public static void PostfixDesiredPowerOutput(CompPowerPlantWater __instance, ref float __result)
+        {
+            // Season
+            __result *= WatermillUtility.SeasonalPowerOutputFactorFor(__instance.parent.GetMapSeason());
+
+            // Outdoor Temperature
+            __result *= WatermillUtility.GetTemperatureToPowerOutputFactorCurveFor(__instance.parent).Evaluate(__instance.parent.MapHeld.mapTemperature.OutdoorTemp);
+
+            // Turbulent Waters
+            if (__instance.parent.MapHeld.GameConditionManager.ConditionIsActive(BWG_GameConditionDefOf.TurbulentWaters))
+                __result *= GameCondition_TurbulentWaters.WatermillPowerGenFactor;
+        }
+
+        public static void PostfixCompInspectStringExtra(CompPowerPlantWater __instance, ref string __result)
+        {
+            // Season
+            Season season = __instance.parent.GetMapSeason();
+            float seasonalPowerProductionFactor = WatermillUtility.SeasonalPowerOutputFactorFor(season);
+
+            __result += "\n" + season.LabelCap() + ": x" + seasonalPowerProductionFactor.ToStringPercent();
+
+            // Outdoor Temperature
+            float tempPowerProductionFactor = WatermillUtility.GetTemperatureToPowerOutputFactorCurveFor(__instance.parent).Evaluate(__instance.parent.MapHeld.mapTemperature.OutdoorTemp);
+            if (tempPowerProductionFactor != 1f)
+                __result += "\n" + "BadTemperature".Translate().CapitalizeFirst() + ": x" + tempPowerProductionFactor.ToStringPercent();
+
+            float turbulentPowerProductionFactor = GameCondition_TurbulentWaters.WatermillPowerGenFactor;
+            if (__instance.parent.MapHeld.GameConditionManager.ConditionIsActive(BWG_GameConditionDefOf.TurbulentWaters))
+                __result += "\n" + BWG_GameConditionDefOf.TurbulentWaters.label.CapitalizeFirst() + ": x" + turbulentPowerProductionFactor.ToStringPercent();
+        }
+
+    }
+}
---
+++ Source/WatermillTweaks/IncidentWorker_TurbulentWaters.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace WatermillTweaks
+{
+    public class IncidentWorker_TurbulentWaters : IncidentWorker_MakeGameCondition
+    {
+
+        public override float BaseChanceThisGame
+        {
+            get
+            {
+                float finalCommonality = 0f;
+                try
+                {
+                    if (map != null)
+                    {
+                        RiverDef river = map.GetRiver();
+                        if (river == RiverDefOf.River)
+                            finalCommonality = def.baseChance;
+                        else if (river == RiverDefOf.LargeRiver)
+                            finalCommonality = def.baseChance * 2f;
+                        else if (river == RiverDefOf.HugeRiver)
+                            finalCommonality = def.baseChance * 4f;
+                        finalCommonality *= RainfallToAdjustedChanceFactorCurve.Evaluate(map.TileInfo.rainfall);
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    finalCommonality = def.baseChance;
+                }
+                return finalCommonality;
+            }
+        }
+
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            Map currentMap = (Map)parms.target;
+            map = currentMap;
+            return !currentMap.TileInfo.Rivers.NullOrEmpty() && currentMap.mapTemperature.SeasonalTemp >= 15f;
+        }
+
+        private static readonly SimpleCurve RainfallToAdjustedChanceFactorCurve = new SimpleCurve
+        {
+            { new CurvePoint(0f, 0f) },
+            { new CurvePoint(200f, 0.5f) },
+            { new CurvePoint(1000f, 1f) },
+            { new CurvePoint(2000f, 1f) },
+            { new CurvePoint(3000f, 1.3f) },
+            { new CurvePoint(5000f, 2f) },
+        };
+
+        private Map map;
+
+    }
+}
